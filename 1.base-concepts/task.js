1b0cd6b42a326dcf7a4b6c2b081b8e1647c5b063
Alina <Alin.k4@yandex.ru>
2022-08-16 21:09:27 +0300

    first commit variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,27 +1,20 @@
 "use strict"
 
 function solveEquation(a, b, c) {
-  let arr;
-  if(a == 0)
-  return false;
-let res = {};
-let D = b * b - 4 * a * c;
-console.log('D = ' + D);
-if(D < 0)
-  return false;
-res['discriminant'] = D;
-if(D == 0)
-  res["quadratic roots"] = (-b + Math.sqrt(D)) / (2 * a);
-else if(D > 0){
-  let tmp = [];
-  tmp.push((-b + Math.sqrt(D)) / (2 * a));
-  tmp.push((-b - Math.sqrt(D)) / (2 * a));
-  res["quadratic roots"] = tmp;
+  if(a == 0) return "a=0 недопустимое значение!";
+  let D = b * b - 4 * a * c; // Дискриминант
+   let x1, x2;
+   if (D > 0) {
+		x1 = (-b + Math.sqrt(D)) / (2 * a);
+		x2 = (-b - Math.sqrt(D)) / (2 * a);
+		return "Коэффициенты: a="+a+", b="+b+", c="+c+"\nКорни уравнения: X1=" + x1 + ", X2=" + x2 + "\nДискриминант(D)=" + D;
+	} else if (D == 0) {
+		x1 = -b / (2 * a);
+		return "Коэффициенты: a="+a+", b="+b+", c="+c+"\nКорень уравнения: X=" + x1 + "\nДискриминант(D)=" + D;
+	} else if (D < 0) return "Коэффициенты: a="+a+", b="+b+", c="+c+"\nКорней нет!\nДискриминант(D)=" + D;
 }
-return res;
-}
-console.log(quadraticEquation(1,12,36));
-  return arr; 
+
+ 
 
 
 
