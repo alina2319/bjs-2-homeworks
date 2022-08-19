bbef6d5d2f44b5d3c74b104e3d92c20093546faa
Alina <Alin.k4@yandex.ru>
2022-08-19 22:23:18 +0300

    first commit variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,18 +1,21 @@
 "use strict"
 
 function solveEquation(a, b, c) {
-  if(a == 0) return "a=0 недопустимое значение!";
-  let D = b * b - 4 * a * c; // Дискриминант
-   let x1, x2;
-   if (D > 0) {
-		x1 = (-b + Math.sqrt(D)) / (2 * a);
-		x2 = (-b - Math.sqrt(D)) / (2 * a);
-		return "Коэффициенты: a="+a+", b="+b+", c="+c+"\nКорни уравнения: X1=" + x1 + ", X2=" + x2 + "\nДискриминант(D)=" + D;
-	} else if (D == 0) {
-		x1 = -b / (2 * a);
-		return "Коэффициенты: a="+a+", b="+b+", c="+c+"\nКорень уравнения: X=" + x1 + "\nДискриминант(D)=" + D;
-	} else if (D < 0) return "Коэффициенты: a="+a+", b="+b+", c="+c+"\nКорней нет!\nДискриминант(D)=" + D;
+  let arr = [];  
+  let d = (b ** 2) - (4 * a * c);
+	if (d < 0) {
+		arr.push();
+	} else if (d == 0) {		
+		let x1 = -b / (2 * a);
+			arr.push(x1);
+	} else if (d > 0) {
+		let x1 = (-b + Math.sqrt(d))/(2 * a);
+		let x2 = (-b - Math.sqrt(d))/(2 * a);
+			arr.push(x1, x2);		  		
+	} 
+  return arr; 
 }
+
 
  
 
