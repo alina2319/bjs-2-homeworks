ad9a18c379331f85302afb2b5c674fdcd74337b0
Alina <Alin.k4@yandex.ru>
2022-08-20 13:28:06 +0300

    first commit variables

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -20,23 +20,36 @@ function getArrayParams(arr) {
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
+  let max = 0;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    func(arrOfArr[i]);
+      if (func(arrOfArr[i]) > max) {
+        max = func(arrOfArr[i]);
+      }
+  }
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let max = arr[0];
+  let min = arr[0];
+  let diff = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > max) {
+        max = arr[i];
+      }
+      if (arr[i] < min) {
+        min = arr[i];
+      }
+    }
+  return Math.abs(max - min);
 }
